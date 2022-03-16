084eaeee601a6fad46369bd851937a77bb14d3c6
yvan <https://github.com/Pallies>
2022-03-16 17:10:45 +0100

    function

--- src/test/java/java8/ex07/Function_07_Test.java
+++ src/test/java/java8/ex07/Function_07_Test.java
@@ -14,23 +14,32 @@ public class Function_07_Test {
     // TODO ex. "(10+11)=21", "(5-2)=3"
     String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
         // TODO
-        return null;
+        return new StringBuilder("(")
+                .append(nb1)
+                .append(symbol)
+                .append(nb2)
+                .append(")=")
+                .append(operator.applyAsInt(nb1,nb2))
+                .toString();
     }
     // end::format[]
 
     // TODO définir sum pour que le test test_format_sum() soit passant
-    IntBinaryOperator sum = null;
+    IntBinaryOperator sum = (a,b) -> a + b;
+    IntBinaryOperator sum2 = Integer::sum;
 
     @Test
     public void test_format_sum() throws Exception {
 
         String result = format(12, 13, "+", sum);
+        String result2 = format(12, 13, "+", sum2);
 
         assert result.equals("(12+13)=25");
+        assert result2.equals("(12+13)=25");
     }
 
     // TODO définir substract afin que le test test_format_subtract() soit passant
-    IntBinaryOperator substract = null;
+    IntBinaryOperator substract = (a,b)->a-b;
 
     @Test
     public void test_format_subtract() throws Exception {

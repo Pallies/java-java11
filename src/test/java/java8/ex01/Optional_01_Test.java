132ae57fa5a43d009ecd8d77fd31117a262ca580
yvan <https://github.com/Pallies>
2022-03-16 17:21:24 +0100

    function

--- src/test/java/java8/ex01/Optional_01_Test.java
+++ src/test/java/java8/ex01/Optional_01_Test.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import org.junit.Test;
 
@@ -16,35 +17,35 @@ import java8.data.Person;
  * Exercice 02 - Filter, Map
  */
 public class Optional_01_Test {
-	
-	class NotPresentException extends RuntimeException {
-		
-	}
+
+    class NotPresentException extends RuntimeException {
+
+    }
 
     @Test
     public void test_optional_ifPresent() throws Exception {
-    	
-    	List<Person> persons = Data.buildPersonList(100);
 
+        List<Person> persons = Data.buildPersonList(100);
+        Predicate<Person> adult = (Person p) -> p.getAge() >= 18;
         // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 18 ans
         // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream().filter(adult).findFirst();
         assertThat(optPerson.isPresent(), is(true));
-        
+
         // TODO afficher la personne en question si l'optional contient une personne
     }
 
-    @Test(expected=NotPresentException.class)
+    @Test(expected = NotPresentException.class)
     public void test_optional_notPresent() throws Exception {
-    	List<Person> persons = Data.buildPersonList(50);
+        List<Person> persons = Data.buildPersonList(50);
 
         // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 75 ans
         // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream().filter(p->p.getAge()==75).findFirst();
         assertThat(optPerson.isPresent(), is(false));
-        
+
         // TODO si la personne n'existe pas, jeter une exception NotPresentException
         // TODO utiliser la méthode "orElseThrow"
-
+        persons.stream().filter(p->p.getAge()==75).findFirst().orElseThrow(NotPresentException::new);
     }
 }
